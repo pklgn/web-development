81a7ed4bfa237dca0ad4a4517936db50c3bbb3ad
pklgn <petechka_petrov_201@mail.ru>
2021-03-03 18:14:19 +0300

    change wrong blank

--- lw2/task2.pas
+++ lw2/task2.pas
@@ -2,20 +2,20 @@ PROGRAM SarahRevere(INPUT, OUTPUT);
 USES
   GPC;
 VAR
-  Query1: STRING;
+  QueryValue: STRING;
 BEGIN {SarahRevere}
   WRITELN('Content-Type: text/plain');
   WRITELN;
-	Query1 := GetEnv('QUERY_STRING');
-  IF Query1 = 'lanterns=1'
+  QueryValue := GetEnv('QUERY_STRING');
+  IF QueryValue = 'lanterns=1'
   THEN
     WRITELN('The British are coming by land.')
   ELSE
-    IF Query1 = 'lanterns=2'
+    IF QueryValue = 'lanterns=2'
     THEN
       WRITELN('The British are coming by sea.')
     ELSE
-      IF Query1 = 'lanterns=3'
+      IF QueryValue = 'lanterns=3'
       THEN
         WRITELN('The British are coming by air.')
       ELSE
