42a4ec376cb3b19c6d7cd7701c3a863897b54627
pklgn <petechka_petrov_201@mail.ru>
2021-04-24 17:18:13 +0300

    add calc function

--- lw8/task2/js/calc.js
+++ lw8/task2/js/calc.js
@@ -13,6 +13,8 @@ function makeSimpleCalc(sign, leftOperand, rightOperand) {
         case '/':
             result = leftOperand / rightOperand;
             break;
+        default:
+            result = undefined;
     }
     return result;
 }
@@ -37,11 +39,12 @@ function arrayExp(exp) {
             possibleNumber = exp.split(/[-\/*+\s()]/, 1)[0];
             isNaN(possibleNumber) ? valueErr = true : (
                 arr.push(+possibleNumber),
-                extraElm -=1
+                    extraElm -=1
             );
             exp = exp.slice(possibleNumber.length);
         } else if (brackets.includes(currentSymbol)) {
             brackets.indexOf(currentSymbol) >= (brackets.length / 2) ? extraBrackets += 1 : extraBrackets -= 1;
+            arr.push(currentSymbol);
             exp = exp.substring(1);
         } else {
             valueErr = true;
@@ -56,19 +59,42 @@ function arrayExp(exp) {
 
 function makeCalc(usableArray) {
     let possibleActions = ['*', '/', '+', '-'];
+    let brackets = ['(', ')'];
     let leftVal, rightVal, operator, expVal;
+    let intoBrackets = false;
     operator = usableArray.splice(0, 1)[0];
+    if (!possibleActions.includes(operator)) {
+        if (operator === '(') {
+            intoBrackets = true;
+            operator = usableArray.splice(0, 1)[0];
+        } else {
+            operator = undefined;
+        }
+    }
     leftVal = usableArray[0];
-    possibleActions.includes(leftVal) ? leftVal = makeCalc(usableArray) : usableArray.splice(0, 1);
+    if (possibleActions.includes(leftVal) || brackets.includes(leftVal)) {
+        leftVal = makeCalc(usableArray);
+    } else {
+        usableArray.splice(0, 1);
+    }
     rightVal = usableArray[0];
-    possibleActions.includes(rightVal) ? rightVal = makeCalc(usableArray) : usableArray.splice(0, 1);
+    if (possibleActions.includes(rightVal) || brackets.includes(rightVal)) {
+        rightVal = makeCalc(usableArray);
+    } else {
+        usableArray.splice(0, 1);
+    }
     expVal = makeSimpleCalc(operator, leftVal, rightVal);
+    if (usableArray[0] === ')' && intoBrackets) {
+        usableArray.splice(0, 1);
+    } else if (intoBrackets) {
+        expVal = undefined;
+    }
     return expVal;
 }
 
 function calc(inputExp) {
     let expressionArray;
-    let answer;
+    let answer = null;
     expressionArray = arrayExp(inputExp);
     if (expressionArray.length) {
         answer = makeCalc(expressionArray);
@@ -82,7 +108,7 @@ function calc(inputExp) {
             }
         }
     } else {
-        console.log('Wrong expression');
+        console.log('There are extra or wrong values in the expression ');
     }
-    return true;
+    return answer;
 }
