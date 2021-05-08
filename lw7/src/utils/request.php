076b9fcb60af2348188e4963ab773ee7fc453eb1
pklgn <petechka_petrov_201@mail.ru>
2021-05-08 19:39:10 +0300

    add utils

--- lw7/src/utils/request.php
+++ lw7/src/utils/request.php
@@ -2,19 +2,19 @@
 
 function getPostParameter(string $value): string
 {
-    isset($value) ? $param = $_POST[$value] : $param = null;
+    isset($_POST[$value]) ? $param = $_POST[$value] : $param = "";
     return $param;
 }
 
 function getGetParameter(string $value): string
 {
-    isset($value) ? $param = $_POST[$value] : $param = null;
+    isset($_POST[$value]) ? $param = $_POST[$value] : $param = "";
     return $param;
 }
 
 function getRequestMethod(): string
 {
     $method = $_SERVER['REQUEST_METHOD'];
-    return $method;
+    return strtolower($method);
 }
 
--- lw7/src/utils/template.php
+++ lw7/src/utils/template.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @param string $tplName
+ * @param array $args
+ */
 function renderTemplate(string $tplName, array $args = []): void
 {
     include __DIR__ . "/../templates/$tplName";
